753113f2b0e17b5f959e2fd1dd62456ebe0a21d7
tienlam <lamtien98@gmail.com>
2022-10-04 21:43:31 +0700

    c03

---
+++ java.js
@@ -0,0 +1,91 @@
+// bai1
+// function ketqua() {
+//     let diemTB = 1;
+//     diemTB = Number(prompt("Nhap diem cuoi khoa:"));
+//     if (diemTB >= 5)
+//         console.log("Dau");
+//     else console.log("rot");
+// }
+// ketqua();
+
+// bai2
+// function testPowerNumber() {
+//     let powerNumber = Number(prompt("Nhap so ban muon"));
+//     if ((powerNumber % 10) == 9) {
+//         console.log("Yes");
+//     }
+//     else console.log("No");
+// }
+// testPowerNumber();
+
+// bai3
+// function tamGiacCan() {
+//     let a = Number(prompt("nhap chieu dai canh 1")), 
+//     b = Number(prompt("nhap chieu dai canh 2")),
+//     c = Number(prompt("nhap chieu dai canh 3"));
+// if (a == b || b == c || c == a) {
+//     console.log("Tam giac can");
+// }
+// else console.log("tam giac khong can");
+// }
+// tamGiacCan();
+
+// bai4
+// function tienDien() {
+//     let kw = Number(prompt("Nhap so dien"));
+// if (kw < 100) 
+//     console.log("Tien dien cua ban la", kw * 2000);
+// else if (kw < 200)
+//     console.log("Tien dien cua ban la", kw * 2500);
+//     else console.log("tien dien cua ban la", kw * 3500);
+// }
+// tienDien();
+
+// bai5
+
+// bai6
+// function phTrinhBac1() {
+//     let a = Number(prompt("nhap so a")), b = Number(prompt("nhap so b"));
+//     if (a == 0)
+//         if (b == 0) {
+//             console.log("Phuong trinh co vo so nghiem");
+//         }
+//         else console.log("Phuong trinh vo nghiem");
+//     else {
+//         let x = (-b)/a;
+//         console.log("Nghiem cua phuong trinh la", x);
+//     }
+// }
+// phTrinhBac1();
+
+// bai7
+// function phTrinhBac2() {
+//     let a = Number(prompt("Nhap so a")),
+//         b = Number(prompt("Nhap so b")),
+//         c = Number(prompt("Nhap so c"));
+// if (a == 0) {
+//     if (b == 0) {
+//         if (c == 0) {
+//             console.log("PT co vo so nghiem");
+//         }
+//         else console.log("PT vo nghiem");
+//     }
+//     else {
+//         x = (-c)/b;
+//         console.log("Nghiem cua PT la", x);
+//     }
+// }
+// else {
+//     let delta = b**2-4*a*c;
+//     console.log("Nghiem so 1 cua PT", (-b+Math.sqrt(delta))/(2*a));
+//     console.log("Nghiem so 2 cua PT", (-b-Math.sqrt(delta))/(2*a))
+// }
+// }
+// phTrinhBac2();
+
+// bai8
+
+
+
+
+
