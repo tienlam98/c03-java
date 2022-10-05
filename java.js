d1daf2d5a30e3b75e1b14901077262dd588c02ac
tienlam <lamtien98@gmail.com>
2022-10-05 17:50:24 +0700

    c03 v2

--- java.js
+++ java.js
@@ -42,6 +42,24 @@
 // tienDien();
 
 // bai5
+// function getTypeLetter() {
+//     let x = prompt("Nhap ki tu bat ky");
+//     if (x === Number(1,2,3,4,5,6,7,8,9,0)) {
+//         console.log("So");
+//     }
+//     else if (x === String("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")) {
+//         console.log("Hoa");
+//             }
+    
+//         else if (x === String("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")) {
+//             console.log("thuong");
+//         }
+//             else console.log("Ki tu dac biet");
+// }
+// getTypeLetter();
+
+
+
 
 // bai6
 // function phTrinhBac1() {
@@ -84,7 +102,75 @@
 // phTrinhBac2();
 
 // bai8
-
+function readNumber() {
+let x = prompt("Nhap so nguyen hai chu so");
+if (x < 10 || x > 99) {
+    console.log("So khong hop le");
+}
+else {
+    let donVi = x % 10; 
+    let chuc = Math.floor(x / 10);
+    switch (chuc) {
+        case 1:
+            console.log("muoi ");
+            break;
+        case 2:
+            console.log("hai muoi");
+            break;
+        case 3:
+            console.log("ba muoi");
+            break;
+        case 4:
+                console.log("bon muoi");
+                break;
+        case 5:
+            console.log("nam muoi");
+            break;
+        case 6:
+            console.log("sau muoi");
+            break;
+        case 7:
+            console.log("bay muoi");
+            break;
+        case 8:
+            console.log("tam muoi");
+            break;
+        case 9:
+            console.log("chin muoi");
+            break;
+    }
+    switch (donVi) {
+        case 1:
+            console.log("mot");
+            break;
+        case 2:
+            console.log("hai");
+            break;
+        case 3:
+            console.log("ba");
+            break;
+        case 4:
+            console.log("bon");
+            break;
+        case 5:
+            console.log("nam");
+            break;
+        case 6:
+            console.log("sau");
+            break;
+        case 7:
+            console.log("bay");
+            break;
+        case 8:
+            console.log("tam");
+            break;
+        case 9:
+            console.log("chin");
+            break;
+    }
+}
+}
+readNumber();
 
 
 
